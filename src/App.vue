85698015b8a6d031427a2ee430e5d1f7717101c3
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-05-02 14:55:00 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      greetings: "ciao"
+    }
+  }
+}
+</script>
+
+<template>
+<h1>{{ greetings }}</h1>
+</template>
+
+<style scoped>
+
+</style>
