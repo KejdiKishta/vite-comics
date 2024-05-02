176eacda9e1a650ec3671a4395d3d74031736754
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-05-02 15:48:53 +0200

    add: AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,91 @@
 <script>
-
+export default {
+    data() {
+        return {
+            headerNav: [
+                {
+                    item: "CHARACTER",
+                    isActive: false
+                },
+                {
+                    item: "COMICS",
+                    isActive: true
+                },
+                {
+                    item: "MOVIES",
+                    isActive: false
+                },
+                {
+                    item: "TV",
+                    isActive: false
+                },
+                {
+                    item: "GAMES",
+                    isActive: false
+                },
+                {
+                    item: "COLLECTIBLES",
+                    isActive: false
+                },
+                {
+                    item: "VIDEOS",
+                    isActive: false
+                },
+                {
+                    item: "FANS",
+                    isActive: false
+                },
+                {
+                    item: "NEWS",
+                    isActive: false
+                },
+                {
+                    item: "SHOP",
+                    isActive: false
+                }
+            ]
+        }
+    }
+}
 </script>
 
 <template>
-
+    <header>
+        <div><img src="../assets/img/dc-logo.png" alt=""></div>
+        <ul>
+            <li v-for="link in headerNav">
+                <a href="#" :class="{'active': link.isActive}">{{ link.item }}</a>
+            </li>
+        </ul>
+    </header>
 </template>
 
 <style scoped lang="scss">
+    @use "../style/partials/mixin" as *;
+    @use "../style/partials/variables" as *;
+
+    header {
+        @include flex (row, space-around, center);
+        font-size: .8rem;
+        padding: 20px 0;
+
+    }
+    
+    ul {
+        @include flex (row, center, center);
+        list-style-type: none;
+        gap: 10px;
+
+        a {
+            text-decoration: none;
+            color: $black-color;
+            padding: 20px 0;
+        }
+        
+        .active {
+            border-bottom: 3px solid blue;
+            font-weight: bold;
+        }
+    }
 
 </style>
