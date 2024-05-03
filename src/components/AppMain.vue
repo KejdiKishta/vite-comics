8e8211d86773fdd59327017725a85b5b7b1f0873
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-05-03 16:09:09 +0200

    add: props

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,10 +3,89 @@ Continuate a lavorare nella stessa repo di ieri e create un nuovo componente per
 Una volta inseriti tutti i contenuti dinamicamente, completate il vostro layout e rifinite i dettagli con Sass. -->
 
 <script>
+import SeriesProps from "./seriesProps.vue";
+
 export default {
+    components: {
+        SeriesProps
+    },
+
     data() {
         return {
-
+            series: [
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "price": "$19.99",
+                    "series": "ACTION COMICS",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    "price": "$3.99",
+                    "series": "AMERICAN VAMPIRE 1976",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                    "price": "$16.99",
+                    "series": "ACQUAMAN",
+                    "type": "graphic novel"
+                },
+                {
+                    "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                    "price": "$2.99",
+                    "series": "BATGIRL",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                    "price": "$3.99",
+                    "series": "BATMAN",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                    "price": "$2.99",
+                    "series": "BATMAN BEYOND",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                    "price": "$3.99",
+                    "series": "BATMAN/SUPERMAN",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                    "price": "$4.99",
+                    "series": "BATMAN/SUPERMAN ANNUAL",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                    "price": "$5.99",
+                    "series": "BATMAN: THE JOKER WAR ZONE",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                    "price": "$6.99",
+                    "series": "BATMAN: THREE JOKERS",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                    "price": "$4.99",
+                    "series": "BATMAN: WHITE KNIGHT PRESENTS: HARLEY QUINN",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                    "price": "$16.99",
+                    "series": "CATWOMAN",
+                    "type": "graphic novel"
+                }
+            ]
         }
     }
 }
@@ -15,20 +94,27 @@ export default {
 <template>
     <main>
         <div class="container">
-            <h2>Penso di non aver mai visto un film della DC comics &#x1F440;</h2>
+            <seriesProps
+            v-for="card in series"
+            :cardThumb="card.thumb"
+            :cardSeries="card.series"
+            />
         </div>
     </main>
 </template>
 
 <style scoped lang="scss">
     @use "../style/partials/variables" as *;
+    @use "../style/partials/mixin" as *;
 
     main {
         padding: 50px;
         background-color: $black-color;
-        
-        h2 {
-            color: $white-color;
+
+        .container {
+            @include flex (row, space-evenly, center);
+            flex-wrap: wrap;
+
         }
     }
 
---
+++ src/components/seriesProps.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    props: {
+        cardThumb: String,
+        cardPrice: String,
+        cardSeries: String,
+        cardType: String,
+    },
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <div>
+            <img :src="cardThumb" :alt="cardSeries">
+        </div>
+        <h5>{{ cardSeries }}</h5>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    @use "../style/partials/variables" as *;
+
+    .card {
+        width: 15%;
+
+        img {
+            height: 200px;
+        }
+
+        h5 {
+            color:white;
+            font-size: .8rem;
+            height: 40px;
+        }
+    }
+
+</style>
